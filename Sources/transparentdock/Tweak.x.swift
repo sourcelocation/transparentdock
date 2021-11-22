ea072c38d8f14addd2ffc4435b57c499290ea528
Matthew Anisovich <anisovich.matthew@gmail.com>
2021-11-22 11:32:33 +0300

    iPad Support

--- Sources/transparentdock/Tweak.x.swift
+++ Sources/transparentdock/Tweak.x.swift
@@ -3,10 +3,18 @@ import UIKit
 import TransparentDockC
 
 
-class DockHook: ClassHook<UIView> {
+class iOSDockHook: ClassHook<UIView> {
     static var targetName: String = "SBDockView"
     
     func layoutSubviews() {
         orig.target.backgroundColor = UIColor(white: 0, alpha: 0)
+    }
+}
+
+class iPadDockHook: ClassHook<UIView> {
+    static var targetName: String = "SBFloatingDockPlatterView"
+    
+    func layoutSubviews() {
+        orig.target.isHidden = true
     }
 }
